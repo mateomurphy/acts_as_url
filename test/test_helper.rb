6a25d599fdb91704a772138d261bc30a6e4db7fa
Mateo Murphy <mateo.murphy@gmail.com>
2016-03-29 23:55:07 -0400

    Get code and tests running with recent ruby
    Use sqlite memory database for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,22 +2,21 @@ ENV['RAILS_ENV'] = 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
 
 require 'test/unit'
-require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
+require 'yaml'
+#require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
 
 # require 'rubygems'
 # require 'active_support'
 # require 'active_support/test_case'
-# require 'active_record'
-# require File.dirname(__FILE__) + '/../init'
+require 'active_record'
+require File.dirname(__FILE__) + '/../init'
 
 
 
 def load_schema
-  config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
-  ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log") 
-  
-  db_adapter = 'mysql'
-  ActiveRecord::Base.establish_connection(config[db_adapter])
+  config = {adapter: 'sqlite3', database: ":memory:" }
+  ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
+  ActiveRecord::Base.establish_connection(config)
   load(File.dirname(__FILE__) + "/schema.rb")
   require File.dirname(__FILE__) + '/../init.rb'
 end
